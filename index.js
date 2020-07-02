1d75a4f93ca2d5407f44668025ba6a1b1e9e36b7
Justin Nand <nandj@oregonstate.edu>
2020-07-01 18:47:41 -0700

    database

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
+let db = new sqlite3.Database('./db/appsploit.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the chinook database.');
+  }
+});
