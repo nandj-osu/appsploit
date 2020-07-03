422fd47cf5f3b75183cd0d18eb6c7958afea64d8
Justin Nand <nandj@oregonstate.edu>
2020-07-02 22:23:36 -0700

    Basic pages

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
+const bodyparser = require("body-parser");
 const sqlite3 = require('sqlite3').verbose();
-
 const app = express();
 
+
+//
+// Configuration
+//
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+app.set("port", process.argv[2]);
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 // 
 // Connect to database
@@ -15,4 +23,39 @@ let db = new sqlite3.Database('./db/appsploit.db', sqlite3.OPEN_READWRITE, (err)
   } else {
     console.log('Connected to the appsploit database.');
   }
+});
+
+
+//
+// Endpoints
+//
+app.get('/', function(req, res, next){
+    let context = {
+        results: []
+    };
+
+    res.render('home', context);
+});
+
+
+//
+// Error Handlers
+//
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.log(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+//
+// Server entry point
+//
+app.listen(app.get('port'), function(){
+    console.log(`Express started on port ${app.get('port')}`);
 });
