602a906a57e690eeaa0e3f6f7713c090495902c0
Justin Nand <nandj@oregonstate.edu>
2020-07-11 20:45:17 -0700

    handle malformed json in vulnerable case

--- routes/insecureDeserialRoutes/routeInsecureDeserialPostTask.js
+++ routes/insecureDeserialRoutes/routeInsecureDeserialPostTask.js
@@ -2,31 +2,44 @@ let db = require("../../db");
 let serialize = require("node-serialize");
 
 const routePostTask = (req, res, next) => {
+    let json;
     if (req.session.secure) {
-        let json = JSON.parse(req.body.desc);
-
-        if (json.desc) {
-            data = [json.desc, 0, req.session.user];
-            sql = "insert into todo(task_description, task_complete, user_id) values(?,?,?)";
-            db.run(sql, data, function (err) {
-                if (err) {
-                    console.error(err.message);
-                }
-            });
-
-            req.session.flash = {
-                message: "JSON successfully processed",
-                class: "alert-success",
-            };
-        } else {
-            req.session.flash = {
-                message: "Invalid json posted",
-                class: "alert-warning",
-            };
-        }
+        json = JSON.parse(req.body.desc);
     } else {
-        let json = serialize.unserialize(req.body.desc);
+        try {
+            json = serialize.unserialize(req.body.desc);
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                req.session.flash = {
+                    message: "Invalid json posted",
+                    class: "alert-warning",
+                };
+            } else {
+                throw e;
+            }
+        }
     }
+
+    if ( typeof json !== 'undefined' && json.desc) {
+        data = [json.desc, 0, req.session.user];
+        sql = "insert into todo(task_description, task_complete, user_id) values(?,?,?)";
+        db.run(sql, data, function (err) {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+
+        req.session.flash = {
+            message: "JSON successfully processed",
+            class: "alert-success",
+        };
+    } else {
+        req.session.flash = {
+            message: "Invalid json posted",
+            class: "alert-warning",
+        };
+    }
+
 
     res.redirect(req.originalUrl);
 };
