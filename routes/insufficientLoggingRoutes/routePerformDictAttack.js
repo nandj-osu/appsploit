683c79cf8a1d4fed8f66bd278844588cec7d44be
Sam Chaara <Sam.R.Chaara@gmail.com>
2020-08-12 17:48:02 -0500

    Quote file path for dict_attack script

--- routes/insufficientLoggingRoutes/routePerformDictAttack.js
+++ routes/insufficientLoggingRoutes/routePerformDictAttack.js
@@ -1,18 +1,19 @@
 //Using node's "exec" function: https://stackabuse.com/executing-shell-commands-with-node-js/
-
 const { exec } = require("child_process");
 const path = require("path");
 const script = path.resolve(__dirname, "../../scripts/dict_attack.py");
 
 const performDictAttack = (req, res, next) => {
     var app_url = `${req.protocol}://${req.get("host")}`;
-    var cmd;
+    var app_mode;
     const target_user = "system_admin";
     if (req.session.secure) {
-        cmd = `python ${script} ${app_url} ${target_user} secure`;
+        app_mode = "secure";
+        cmd = `python "${script}" ${app_url} ${target_user} secure`;
     } else {
-        cmd = `python ${script} ${app_url} ${target_user} unsecure`;
+        app_mode = "unsecure";
     }
+    const cmd = `python "${script}" ${app_url} ${target_user} ${app_mode}`;
 
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
